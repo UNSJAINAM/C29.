30716cd6461cd45ec22a099c2b3e130d046cd66f
UNSJAINAM <85750958+UNSJAINAM@users.noreply.github.com>
2021-09-09 21:26:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,76 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground, bridge;
+var leftWall, rightWall;
+var jointPoint;
+var jointLink;
+var zombie;
+var zombie1, zombie2, zombie3, zombie4;
+var breakButton;
+var backgroundImage;
+
+var stones = [];
+
+function preload() {
+
+
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+  ground = new Base(0, height - 10, width * 2, 20);
+  leftWall = new Base(100, height - 300, 200, height / 2 + 100);
+  rightWall = new Base(width - 100, height - 300, 200, height / 2 + 100);
+
+  bridge = new Bridge(30, { x: 160, y: height / 2 - 140 });
+  jointPoint = new Base(width - 250, height / 2 - 100, 40, 20);
+
+  Matter.Composite.add(bridge.body, jointPoint);
+  jointLink = new Link(bridge, jointPoint);
+
+  for (var i = 0; i <= 8; i++) {
+    var x = random(width / 2 - 200, width / 2 + 300);
+    var y = random(-100, 100);
+    var stone = new Stone(x, y, 80, 80);
+    stones.push(stone);
+  }
+
+  
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+
+  bridge.show();
+  ground.show();
+  leftWall.show();
+  rightWall.show();
+  for (var stone of stones) {
+    stone.show();
+  }
+
+ 
+
+  drawSprites();
+}
+
+function handleButtonPress() {
+  jointLink.dettach();
+  setTimeout(() => {
+    bridge.break();
+  }, 1500);
+}
